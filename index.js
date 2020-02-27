605ff0a91e3f0473f791ec47d3ad17f5cd842707
Bagoes Arinata <45513139+arinata098@users.noreply.github.com>
2020-02-27 14:59:07 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,94 @@
+const electron = require("electron");
+
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+let todayWindow;
+let createWindow;
+let listWindow;
+
+app.on("ready", ()=> {
+    todayWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        title: "Aplikasi dokter2"
+    });
+
+    todayWindow.loadURL(`file://${__dirname}/today.html`);
+    todayWindow.on("close", () => {
+
+        app.quit();
+        todayWindow = null;
+    });
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+
+});
+
+const listWindowCreator = () => {
+    listWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title:"All Appointment"
+    });
+
+    listWindow.setMenu(null);
+    listWindow.loadURL(`file://${__dirname}/list.html`);
+    listWindow.on("close", () => (listWindow = null));
+};
+
+const createWindowCreator = () => {
+    createWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title:"Create Appointment"
+    });
+
+    createWindow.setMenu(null);
+    createWindow.loadURL(`file://${__dirname}/create.html`);
+    createWindow.on("close", () => (createWindow = null));
+};
+
+const menuTemplate = [{
+        label: "File",
+        submenu: [
+            {
+                label: "New Appointment",
+
+                click() {
+                    createWindowCreator();
+                }
+            },
+            {
+                label: "All Appointment",
+
+                click() {
+                    listWindowCreator();
+                }
+            },
+            {
+                label: "Quit",
+                accelerator: process.platform === "darwin" ? "Command + Q" : "Ctrl + Q",
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    },
+
+    {
+
+        label: "View",
+        submenu: [{role: "Reload"}, {role: "toggledevtolls"}]
+    }
+
+
+
+]
